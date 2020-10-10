1e833630a4d363e95e9adf8295e83f1d60164b6c
Paulojosep <paulo.jmodesto@htmail.com>
2020-10-10 09:58:19 -0300

    Criacao da animacao do elevador subindo

---
+++ Assets/Script/Elevator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+
+    public GameObject elevatorPanel;
+
+    void Start()
+    {
+        elevatorPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.U)){
+        }
+
+        if(Input.GetKeyDown(KeyCode.D)){
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "Player"){
+            elevatorPanel.SetActive(true);   
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if(other.tag == "Player") {
+            elevatorPanel.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private bool Subir = false;
+    public GameObject elevador;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Subir = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.U)){
+            if(Subir ==false){
+                elevador = GameObject.Find("Elevador");
+                elevador.GetComponent<Animation>().GetClip("Subir");
+                Subir = true;
+            }
+        }
+    }
+}
